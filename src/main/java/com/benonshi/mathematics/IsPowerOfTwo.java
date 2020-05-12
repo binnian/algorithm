fde29cc0ab94a92bee5d6198994ff423d7173f44
BenOnShi <shibn9510@gmail.com>
2020-05-12 12:12:44 +0800

    类加载顺序    反转整数

--- src/main/java/com/benonshi/mathematics/IsPowerOfTwo.java
+++ src/main/java/com/benonshi/mathematics/IsPowerOfTwo.java
@@ -1,5 +1,8 @@
 package com.benonshi.mathematics;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
 /**
  * 231. 2的幂
  * 给定一个整数，编写一个函数来判断它是否是 2 的幂次方。
@@ -11,7 +14,8 @@ package com.benonshi.mathematics;
 public class IsPowerOfTwo {
 
 
-    public static Boolean isPowerOfTwo(int n) {
+    @Contract(pure = true)
+    public static @NotNull Boolean isPowerOfTwo(int n) {
         return n > 0 && (n & (n - 1)) == 0;
     }
 
