6409259823c67848420638378f5c5362be756f1e
yearcold <shibn9510@gmail.com>
2020-09-29 16:38:34 +0800

    单词中的每个单词反转 多数元素

---
+++ src/main/java/com/benonshi/array/MajorityElement.java
@@ -0,0 +1,34 @@
+package com.benonshi.array;
+
+/**
+ * 169. 多数元素
+ * 给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+ * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/majority-element
+ *
+ * @author BenOniShi
+ * @date 2020/9/29 16:25
+ */
+public class MajorityElement {
+    public static int majorityElement(int[] nums) {
+        int num = 1;
+        int ans = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != ans) {
+                num--;
+            } else {
+                num++;
+            }
+            if (num == 0) {
+                ans = nums[i];
+                num++;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(majorityElement(new int[]{1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 3, 4, 6, 6}));
+    }
+}
