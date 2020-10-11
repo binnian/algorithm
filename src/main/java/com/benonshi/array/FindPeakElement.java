fe4855be9924f3ac5623f613f25fd3d0c4a34edb
BenOnShi <shibn9510@gmail.com>
2020-10-11 19:07:12 +0800

    编写注释

--- src/main/java/com/benonshi/array/FindPeakElement.java
+++ src/main/java/com/benonshi/array/FindPeakElement.java
@@ -9,7 +9,9 @@ import java.util.Vector;
 public class FindPeakElement {
 
     public static int findPeakElement(int[] nums) {
+        // 循环
         for (int i = 1; i < nums.length; i++) {
+            // 如果   i 位置的值大于 前一位 并且大于 后一位
             if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1] && i + 1 < nums.length) {
                 return i;
             }
@@ -19,13 +21,20 @@ public class FindPeakElement {
 
 
     public static int findPeakElementForMid(int[] nums) {
+        // 左指针
         int left = 0;
+        // 邮指针
         int right = nums.length - 1;
+        // 循环
         for (; left < right; ) {
+            // 二分
             int mid = left + (right - left) / 2;
+            //  二分位置大于 mid + 1 位置 表示存在峰值元素
             if (nums[mid] > nums[mid + 1]) {
+                //  右指针指向mid
                 right = mid;
             } else {
+                // 左指针指向mid + 1 位置
                 left = mid + 1;
             }
         }
