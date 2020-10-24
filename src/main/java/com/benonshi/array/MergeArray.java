f695126a0b45a43c7fe3000601440332e5a32cab
yearcold <shibn9510@gmail.com>
2020-10-24 23:55:19 +0800

    合并两个有序数组

---
+++ src/main/java/com/benonshi/array/MergeArray.java
@@ -0,0 +1,28 @@
+package com.benonshi.array;
+
+/**
+ * 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+ * 说明：
+ *
+ * 初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。
+ * 你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-sorted-array
+ * @author BenOniShi
+ * @date 2020/10/24 23:53
+ */
+public class MergeArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int p = m + n - 1;
+
+        while ((p1 >= 0) && (p2 >= 0)) {
+            nums1[p--] = (nums1[p1] < nums2[p2]) ? nums2[p2--] : nums1[p1--];
+        }
+
+        System.arraycopy(nums2, 0, nums1, 0, p2 + 1);
+    }
+
+}
