446c3c6fce3537893edb0baa8a1372618277bafd
yearcold <shibn9510@gmail.com>
2020-10-28 01:06:38 +0800

    反转链表 递归解法

--- src/main/java/com/benonshi/node/ReverseListNode.java
+++ src/main/java/com/benonshi/node/ReverseListNode.java
@@ -13,6 +13,12 @@ public class ReverseListNode {
         if (head == null || head.next ==null){
             return head;
         }
+        /**
+         * reverseList(head.next) 此时最后一层结束递归的条件是head.next == null , head 等于 4 ， 那么它的上一层就是 reverseList(3.next) ， head 等于3 ， 因此后面
+         * head.next.next = head  相当于 3的后面(4)的后面指向3 ， 那就是 3<-4
+         * 然后 head.next = null , 最后renturn node ， 倒数第二层往上返回了 null <- 3 <-4 。
+         * 最顶层通过递归条件 head == null 整体结束
+         */
         ListNode node = reverseList(head.next);
         head.next.next = head;
         head.next = null;
