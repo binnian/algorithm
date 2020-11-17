7d070b7c1aff992b03e0d93d512081923be179ea
BenOnShi <shibn9510@gmail.com>
2020-11-17 18:52:08 +0800

    最长公共前缀

--- src/main/java/com/benonshi/string/LongestCommonPrefix.java
+++ src/main/java/com/benonshi/string/LongestCommonPrefix.java
@@ -17,16 +17,23 @@ public class LongestCommonPrefix {
         if (strs.length == 0) {
             return "";
         }
+        // 设置ans 为数组第一个字符串
         String ans = strs[0];
+        // 外层循环循环数组每一个位置
         for (int i = 1; i < strs.length; i++) {
+            // 内层循环比较每一个字符串的字符是否相等
             int j = 0;
+            // 如果j < ans.length   && < 外层循环控制的每一个字符串长度
             for (; j < strs[i].length() && j < ans.length(); j++) {
+                // 如果当前比较的字符串不相等
                 if (strs[i].charAt(j) != ans.charAt(j)) {
+                    // 退出循环
                     break;
                 }
             }
+            // ans 为 当前字符串 0 开始到 j 的位置
             ans = ans.substring(0, j);
-
+            // 如果比较完之后 ans为空  则直接返回ans
             if (ans.equals("")) {
                 return ans;
             }
