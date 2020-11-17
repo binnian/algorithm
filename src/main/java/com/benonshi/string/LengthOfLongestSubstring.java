d855070a5f97ed00467ad8e569ec742b573e585c
BenOnShi <shibn9510@gmail.com>
2020-11-17 18:51:55 +0800

    无重复字符的最长子串

--- src/main/java/com/benonshi/string/LengthOfLongestSubstring.java
+++ src/main/java/com/benonshi/string/LengthOfLongestSubstring.java
@@ -19,8 +19,11 @@ public class LengthOfLongestSubstring {
         int n = s.length(), ans = 0;
         Map<Character, Integer> map = new HashMap<>();
         for (int end = 0, start = 0; end < n; end++) {
+            // end 开始
             char alpha = s.charAt(end);
+            // 如果map 包含这个key
             if (map.containsKey(alpha)) {
+                //
                 start = Math.max(map.get(alpha), start);
             }
             ans = Math.max(ans, end - start + 1);
@@ -31,7 +34,7 @@ public class LengthOfLongestSubstring {
 
 
     public static void main(String[] args) {
-        System.out.println(lengthOfLongestSubstring("lockkersty"));
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));
     }
 
 }
