ff21a99aa1880f63ad4a97b1f5f952df889c5ce7
BenOniShi <shibn9510@gmail.com>
2021-01-01 11:53:32 +0800

    原地删除排序数组中重复的元素

---
+++ src/main/java/com/benonshi/array/RemoveDuplicates.java
@@ -0,0 +1,42 @@
+package com.benonshi.array;
+
+/**
+ * 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+ *
+ * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array
+ * @Author: BenOnSHI
+ * @Date: 2021/1/1 11:41
+ **/
+public class RemoveDuplicates {
+
+    /**
+     * 双指针
+     * i  j
+     * j 移动到不等于 i 的位置  将 j当前位置的值与  i下一个相同位置的值变换
+     *
+     * @param nums
+     * @return
+     */
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+
+
+    public static void main(String[] args) {
+        int i = removeDuplicates(new int[]{1, 1, 1, 1, 2, 3, 4, 4, 5, 6});
+        System.out.println(i);
+    }
+}
