9a31a97e5a7422e0d3069acd5c8abba1cb99a7dd
BenOniShi <shibn9510@gmail.com>
2021-02-10 18:22:09 +0800

    子数组最大平均数 I

---
+++ src/main/java/com/benonshi/array/FindMaxAverage.java
@@ -0,0 +1,20 @@
+package com.benonshi.array;
+
+/**
+ * 子数组最大平均数 I
+ * 给定 n 个整数，找出平均数最大且长度为 k 的连续子数组，并输出该最大平均数。
+ *
+ * @Author: BenOnSHI
+ * @Date: 2021/2/6 11:46
+ **/
+public class FindMaxAverage {
+
+    public double findMaxAverage(int[] nums, int k) {
+
+
+
+
+        return 0 * 1.0;
+    }
+
+}
