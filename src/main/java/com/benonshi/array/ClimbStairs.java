94266e6b98223e18c6c0d8f9146dcd57fd2562b2
yearcold <shibn9510@gmail.com>
2021-04-22 21:46:10 +0800

    爬楼梯

---
+++ src/main/java/com/benonshi/array/ClimbStairs.java
@@ -0,0 +1,22 @@
+package com.benonshi.array;
+
+/**
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * 注意：给定 n 是一个正整数。
+ *
+ * @author BenOniShi
+ * @date 2021/4/22 21:22
+ */
+public class ClimbStairs {
+
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
