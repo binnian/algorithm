4700b9b610fc48bab1766f74fb92bdc4e0aac5ec
BenOniShi <shibn9510@gmail.com>
2021-05-22 17:13:22 +0800

    黑板异或游戏

--- src/main/java/com/benonshi/mathematics/XORGame.java
+++ src/main/java/com/benonshi/mathematics/XORGame.java
@@ -12,7 +12,6 @@ package com.benonshi.mathematics;
  * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/chalkboard-xor-game
- * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  *
  * @Author: BenOnSHI
  * @Date: 2021/5/22 11:36
