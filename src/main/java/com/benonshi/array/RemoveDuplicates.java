4a98557780eb63d13c42613f538e9e8a4276e7ae
BenOniShi <shibn9510@gmail.com>
2021-05-30 13:56:04 +0800

    删除有序数组中的重复项

--- src/main/java/com/benonshi/array/RemoveDuplicates.java
+++ src/main/java/com/benonshi/array/RemoveDuplicates.java
@@ -1,6 +1,8 @@
 package com.benonshi.array;
 
+
 /**
+ * 26. 删除有序数组中的重复项
  * 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
  * <p>
  * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
@@ -41,6 +43,7 @@ public class RemoveDuplicates {
 
     /**
      * 删除排序数组中的重复元素 k 问题求解
+     *
      * @param nums
      * @param k
      * @return
@@ -52,7 +55,7 @@ public class RemoveDuplicates {
         }
         int x = 0;
         for (int num : nums) {
-            if (x < k || nums[x-k] != num) {
+            if (x < k || nums[x - k] != num) {
                 nums[x++] = num;
             }
         }
