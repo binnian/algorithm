ec5b650c5a52d487df1fb220e1ddd31d48c72c14
yearcold <shibn9510@gmail.com>
2021-06-07 23:20:06 +0800

    目标和

---
+++ src/main/java/com/benonshi/dp/FindTargetSumWays.java
@@ -0,0 +1,36 @@
+package com.benonshi.dp;
+
+/**
+ * 494. 目标和
+ * 背包问题
+ * 给你一个整数数组 nums 和一个整数 target 。
+ * <p>
+ * 向数组中的每个整数前添加 '+' 或 '-' ，然后串联起所有整数，可以构造一个 表达式 ：
+ * <p>
+ * 例如，nums = [2, 1] ，可以在 2 之前添加 '+' ，在 1 之前添加 '-' ，然后串联起来得到表达式 "+2-1" 。
+ * 返回可以通过上述方法构造的、运算结果等于 target 的不同 表达式 的数目。
+ *
+ * @author BenOniShi
+ * @date 2021/6/7 23:17
+ */
+public class FindTargetSumWays {
+
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum < target || (sum + target) % 2 == 1) {
+            return 0;
+        }
+        int neg = (sum + target) / 2;
+        int[] dp = new int[neg + 1];
+        dp[0] = 1;
+        for (int num : nums) {
+            for (int j = neg; j >= num; j--) {
+                dp[j] += dp[j - num];
+            }
+        }
+        return dp[neg];
+    }
+}
